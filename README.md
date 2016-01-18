8cca275a4454ecdfc0c9d71ff95f631b3893cdd9
codethejason <ihaveapet1@hotmail.com>
2016-01-18 15:30:26 -0500

    Can now send from Gmail

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 #Django Email Sender
 Sends emails in Django as part of Google Code-in 2015.
+
+##Helpful Resources
+[https://docs.djangoproject.com/en/1.9/intro/tutorial01/](https://docs.djangoproject.com/en/1.9/intro/tutorial01/)
+[https://docs.djangoproject.com/en/1.9/topics/email/](https://docs.djangoproject.com/en/1.9/topics/email/)  
--- send_email/views.py
+++ send_email/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
 # Create your views here.
-from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'index.html')
 
 def done(request):
+    email = request.POST.get('email', '')
+    content = "Hello there, the email came through! Be sure to checkout FOSSASIA!"
+    send_mail('It Works!', content, 'from@example.com',
+    [email], fail_silently=False)
     return render(request, 'done.html')

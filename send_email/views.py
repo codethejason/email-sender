3610446f69edb4178b0efe84a9e2718bea08d484
codethejason <ihaveapet1@hotmail.com>
2016-01-19 16:30:24 -0500

    added email validator and subscribe checkbox

--- send_email/views.py
+++ send_email/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 # Create your views here.
 
@@ -10,7 +12,13 @@ def index(request):
 
 def done(request):
     email = request.POST.get('email', '')
+    confirm = request.POST.get('checkbox', '')
+    try:
+        validate_email(email)
+        except ValidationError as e:
+        return render(request, 'error.html')
     content = "Hello there, the email came through! Be sure to checkout FOSSASIA!"
-    send_mail('It Works!', content, 'from@example.com',
-    [email], fail_silently=False)
-    return render(request, 'done.html')
+    if confirm == 'on':
+      send_mail('It Works!', content, 'from@example.com',
+      [email], fail_silently=False)
+      return render(request, 'done.html')
